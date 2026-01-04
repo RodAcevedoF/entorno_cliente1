8dff6eded0e054ee9c2d8f44f020f98274be630e
Rodrigo Acevedo <raacevedof@gmail.com>
2026-01-04 22:38:31 +0100

    fix imports

--- src/features/home/benefits.ts
+++ src/features/home/benefits.ts
@@ -1,23 +1,27 @@
+import img1 from '@/assets/home_01.webp';
+import img2 from '@/assets/home_02.webp';
+import img3 from '@/assets/home_03.webp';
+
 export const BENEFITS = [
 	{
 		id: '01',
 		title: 'Deep Relaxation',
 		description:
 			'Release physical tension and mental stress as you drift into a state of profound calm and equilibrium.',
-		image: '/src/assets/home_01.webp',
+		image: img1,
 	},
 	{
 		id: '02',
 		title: 'Inner Clarity',
 		description:
 			'Clear the noise of daily life to reveal the intuitive wisdom that resides within your core consciousness.',
-		image: '/src/assets/home_02.webp',
+		image: img2,
 	},
 	{
 		id: '03',
 		title: 'Higher Vibration',
 		description:
 			'Elevate your energetic frequency to attract abundance, harmony, and deeper connections with the universe.',
-		image: '/src/assets/home_03.webp',
+		image: img3,
 	},
 ];
