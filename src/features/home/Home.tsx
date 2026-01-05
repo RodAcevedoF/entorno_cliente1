9533cac2f402dd3547ff5a6c0e38dbc108626c07
Rodrigo Acevedo <raacevedof@gmail.com>
2026-01-06 00:44:08 +0100

    add intersection observer

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -3,36 +3,11 @@ import bgImage from '@/assets/vd_bg.webp';
 import { BENEFITS } from './benefits';
 import { BenefitCard } from './components/BenefitCard';
 import { Hero } from './components/Hero';
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
+import { InView } from 'react-intersection-observer';
 
 const Home = () => {
 	const [activeId, setActiveId] = useState<string>('');
-	const observerRefs = useRef<(HTMLElement | null)[]>([]);
-
-	useEffect(() => {
-		const options = {
-			root: null,
-			rootMargin: '-45% 0px -45% 0px',
-			threshold: 0,
-		};
-
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					const id = entry.target.getAttribute('data-id');
-					if (id) setActiveId(id);
-				}
-			});
-		}, options);
-
-		observerRefs.current.forEach((ref) => {
-			if (ref) observer.observe(ref);
-		});
-
-		return () => {
-			observer.disconnect();
-		};
-	}, []);
 
 	return (
 		<div className={styles.pageWrapper}>
@@ -48,23 +23,29 @@ const Home = () => {
 			<Hero />
 
 			{BENEFITS.map((item, index) => (
-				<section
+				<InView
 					key={item.id}
-					data-id={item.id}
-					data-active={activeId === item.id}
-					ref={(el: HTMLElement | null) => {
-						observerRefs.current[index] = el;
-					}}
-					className={`${styles.benefitRow} ${
-						index % 2 !== 0 ? styles.reverse : ''
-					}`}>
-					<div className={styles.numberWrapper}>
-						<span className={styles.bigNumber}>{item.id}</span>
-					</div>
-					<div className={styles.cardWrapper}>
-						<BenefitCard item={item} isActive={activeId === item.id} />
-					</div>
-				</section>
+					threshold={0.5}
+					onChange={(inView) => {
+						if (inView) setActiveId(item.id);
+					}}>
+					{({ ref }) => (
+						<section
+							ref={ref}
+							data-id={item.id}
+							data-active={activeId === item.id}
+							className={`${styles.benefitRow} ${
+								index % 2 !== 0 ? styles.reverse : ''
+							}`}>
+							<div className={styles.numberWrapper}>
+								<span className={styles.bigNumber}>{item.id}</span>
+							</div>
+							<div className={styles.cardWrapper}>
+								<BenefitCard item={item} isActive={activeId === item.id} />
+							</div>
+						</section>
+					)}
+				</InView>
 			))}
 		</div>
 	);
